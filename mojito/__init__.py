beb6f3b267ed7b2610d192c5208efdc746d66b35
abauman97 <austintbauman@outlook.com>
2024-08-25 13:40:13 -0400

    Modify imports

--- mojito/__init__.py
+++ mojito/__init__.py
@@ -1,10 +1,11 @@
-from .app import Mojito as Mojito, AppRouter as AppRouter, redirect_to as redirect_to
+from .app import Mojito as Mojito, redirect_to as redirect_to
+from .routing import AppRouter as AppRouter
 from .helpers import (
     get_flashed_messages as get_flashed_messages,
     flash_message as flash_message,
 )
 from .globals import g as g
-from starlette.responses import (
+from .responses import (
     Response as Response,
     HTMLResponse as HTMLResponse,
     PlainTextResponse as PlainTextResponse,
@@ -13,6 +14,7 @@ from starlette.responses import (
     StreamingResponse as StreamingResponse,
     FileResponse as FileResponse,
 )
-from starlette.templating import Jinja2Templates as Jinja2Templates
-from starlette.requests import Request as Request
-from starlette.testclient import TestClient as TestClient
+from .templating import Jinja2Templates as Jinja2Templates
+from .requests import Request as Request
+from .testclient import TestClient as TestClient
+from .staticfiles import StaticFiles as StaticFiles
