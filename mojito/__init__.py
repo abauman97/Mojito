b09d3d098cd98c78c039e97abb15bccd907801c0
= <austintbauman@outlook.com>
2024-09-03 09:55:04 -0400

    feat(forms): Add form processing function
    Add Form() function that processes a request.body form request against a Pydantic model for validating and typing form inputs.

--- mojito/__init__.py
+++ mojito/__init__.py
@@ -32,3 +32,4 @@ from .routing import AppRouter as AppRouter
 from .routing import redirect_to as redirect_to
 from .staticfiles import StaticFiles as StaticFiles
 from .templating import Jinja2Templates as Jinja2Templates
+from .form import Form as Form
