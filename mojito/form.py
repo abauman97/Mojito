ff8cd7987ece2529c00125d3eeca45b04a63fd5c
= <austintbauman@outlook.com>
2024-09-06 15:29:51 -0400

    feat(forms): Make Form function async context manager
    Convert Form() function into an async context generator so that the request.form() can still be used to work with any files that may have been uploaded.

--- mojito/form.py
+++ mojito/form.py
@@ -1,4 +1,9 @@
-from typing import TypeVar
+from contextlib import asynccontextmanager
+from typing import Any, TypeVar
+
+from pydantic import GetCoreSchemaHandler
+from pydantic_core import CoreSchema, core_schema
+from starlette.datastructures import UploadFile as StarletteUploadFile
 
 from .requests import Request
 
@@ -13,7 +18,8 @@ except ModuleNotFoundError:
 PydanticModel = TypeVar("PydanticModel", bound=BaseModel)
 
 
-async def Form(request: Request, model: type[PydanticModel]) -> PydanticModel:
+@asynccontextmanager
+async def Form(request: Request, model: type[PydanticModel]):
     """Read form data from the request and validate it's content against a Pydantic model
     and return the valid Pydantic model. Extra data in the form is ignored and not passed into the
     Pydantic model. This does not work for processing files. You must use the request directly to get and read
@@ -32,4 +38,19 @@ async def Form(request: Request, model: type[PydanticModel]) -> PydanticModel:
     """
     async with request.form() as form:
         valid_model = model.model_validate(dict(form.items()))
-    return valid_model
+        yield valid_model  # Yield result while in context of request.form()
+
+
+class UploadFile(StarletteUploadFile):
+    """An uploaded file included as part of the request data.
+
+    This is a subclass of starlette.datastructures.UploadFile that can be used in a Pydantic
+    BaseModel class. Pydantic will pass the model through as-is without validation.
+    """
+
+    @classmethod
+    def __get_pydantic_core_schema__(
+        cls, source_type: Any, handler: GetCoreSchemaHandler
+    ) -> CoreSchema:
+        # Allow this file type to pass through pydantic without schema validation
+        return core_schema.any_schema()
