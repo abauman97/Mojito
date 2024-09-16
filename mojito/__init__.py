e1667c859db5e9a61c88063d7f3d0d46affb8e03
= <austintbauman@outlook.com>
2024-09-16 14:47:40 -0400

    Bump to 0.1.1 release

--- mojito/__init__.py
+++ mojito/__init__.py
@@ -1,6 +1,6 @@
 """Mojito framework. Fresh, fast, and simple web framework for building HTML-first websites"""
 
-__version__ = "0.1.0a6"
+__version__ = "0.1.1"
 
 from .app import Mojito as Mojito
 from .globals import g as g
